5ad39a7cd81ed66a1d645547f300268280f308aa
Bryan Samuel <bryansamuelangka@gmail.com>
2026-04-07 17:28:42 +0700

    Feat: MVP View of Similarity View

--- ConnectA/Similarity/SimilarityView.swift
+++ ConnectA/Similarity/SimilarityView.swift
@@ -5,3 +5,37 @@
 //  Created by Michael David Sin on 06/04/26.
 //
 
+import SwiftUI
+
+struct SimilarityView: View {
+    var body: some View {
+        VStack {
+            Text("Your Similarities are...").bold()
+            Button("Still None") {
+                
+            }.disabled(true).background(Color.green).foregroundColor(.white)
+            HStack {
+                Button(action: {
+                    print("Chevron Left Tapped")
+                }) {
+                    Image(systemName: "chevron.left").foregroundColor(.black)
+                    
+                }
+                Text("Edit your interests first to get similarity!")
+                Button(action: {
+                    print("Chevron Right Tapped")
+                }) {
+                    Image(systemName: "chevron.right").foregroundColor(.black)
+                }
+            }
+            Button("Reset Intrests") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SimilarityView()
+}
