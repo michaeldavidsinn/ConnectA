8e847cb8490abbe4935022fe8ff05b01f37960f5
natgwk02 <nwidjaja01@student.ciputra.ac.id>
2026-04-07 17:28:26 +0700

    style: mengganti warna button

--- ConnectA/Connect/ConnectView.swift
+++ ConnectA/Connect/ConnectView.swift
@@ -1,29 +1,5 @@
 import SwiftUI
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        
-        let r, g, b: UInt64
-        switch hex.count {
-        case 6: // 00B4D8
-            (r, g, b) = ((int >> 16), (int >> 8 & 0xFF), (int & 0xFF))
-        default:
-            (r, g, b) = (1, 1, 1)
-        }
-        
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue: Double(b) / 255,
-            opacity: 1
-        )
-    }
-}
-
 struct ConnectView: View {
     
     var body: some View {
@@ -42,6 +18,11 @@ struct ConnectView: View {
                 .padding(.top, 5)
                 
                 // amplop
+                Image("CloseEnvelope")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .frame(maxWidth: .infinity)
                 
                 // similarity
                 Text("Find what connects you")
@@ -80,14 +61,11 @@ struct ConnectButton: View {
                 .foregroundColor(.white)
                 .frame(maxWidth: .infinity)
                 .padding()
-                .background(
-                    Color(hex: "00B4D8")
-                )
+                .background(Color.blue)
                 .clipShape(Capsule())
         }
     }
 }
-
 #Preview{
     ConnectView()
 }
