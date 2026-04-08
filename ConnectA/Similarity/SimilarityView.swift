15f25fac28140aa6f9216e2006050c8bdff04d3a
Bryan Samuel <bryansamuelangka@gmail.com>
2026-04-08 15:41:14 +0700

    style: edit similarity view cardstack

--- ConnectA/Similarity/SimilarityView.swift
+++ ConnectA/Similarity/SimilarityView.swift
@@ -23,30 +23,46 @@ struct SimilarityView: View {
                 .padding(.horizontal, 30)
                 .padding(.vertical, 12).background(
                     Color.gray
-            )
-            .clipShape(Capsule())
-            .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
-
+                )
+                .clipShape(Capsule())
+                .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
+            
             Spacer()
+            
+            //Card stack
             HStack {
+                //Left button
                 Button(action: {
                     print("Chevron Left Tapped")
                 }) {
                     Image(systemName: "chevron.left").foregroundColor(.black)
-                    
-                }
+                }.disabled(true)
+                
+                //Question card
                 Text("Edit your interests first to get similarity!")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(height: 120)
+                    .background(Color.white.opacity(0.8))
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
+                    .padding(.horizontal)
+                
+                //Right Button
                 Button(action: {
                     print("Chevron Right Tapped")
                 }) {
                     Image(systemName: "chevron.right").foregroundColor(.black)
-                }
+                }.disabled(true)
             }
+            
             Spacer()
+            
             Button("Reset Intrests") {
                 
             }
-
+            
         }
         .padding()
     }
