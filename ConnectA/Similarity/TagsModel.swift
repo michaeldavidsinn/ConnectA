9d1e168c7f078038cf9e56943e2ee804facee27d
Bryan Samuel <bryansamuelangka@gmail.com>
2026-04-09 14:11:22 +0700

    Feat: add Tags Model

--- ConnectA/Similarity/TagsModel.swift
+++ ConnectA/Similarity/TagsModel.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Michael David Sin on 06/04/26.
 //
+import SwiftUI
 
+struct Tag: Codable, Identifiable, Hashable {
+    let id = UUID()
+    let category: String
+    let questions: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case category
+        case questions
+    }
+}
