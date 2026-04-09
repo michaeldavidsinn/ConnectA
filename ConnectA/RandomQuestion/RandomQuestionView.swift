92e610a50fc1058bc984f1a10220f487629e1cea
Michael David Sin <146686816+michaeldavidsinn@users.noreply.github.com>
2026-04-09 17:46:13 +0700

    fix: nambah state buat iscongratulate di randomquestionview feat: pindah isi congratulationsview ke randomquestionview feat: tambah randomquestionservice refactor: pindah isi function panggil json ke randomquestionviewmodel dari randomquestionviewmodel nya sendiri ke randomquestionservice

--- ConnectA/RandomQuestion/RandomQuestionView.swift
+++ ConnectA/RandomQuestion/RandomQuestionView.swift
@@ -11,107 +11,137 @@ struct RandomQuestionView: View {
     
     var body: some View {
         ZStack {
-            VStack(spacing: 30) {
+            
+            if viewModel.isFinished {
                 
-                Text(isOpen ? "" : "Click To Open!")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .fontWeight(.bold)
-                    .transition(.opacity)
-                    .offset(x: 0, y: 80)
+                VStack(spacing: 30) {
+                    
+                    Spacer()
+                    
+                    Text("Congratulations!")
+                        .font(.system(size: 32, weight: .bold, design: .rounded))
+                        .shadow(color: .black.opacity(0.1), radius: 5)
+                    
+                    Text(congratBodyText)
+                        .font(.system(size: 20))
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 40)
+                        .lineSpacing(4)
+                    
+                    Spacer()
+                    Spacer()
+                    
+                }
+                .transition(.asymmetric(insertion: .opacity, removal: .opacity))
                 
-                ZStack {
-                    if !isOpen {
-                        
-                        Image("CloseEnvelopeFrame")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 420, height: 420)
-                            .onTapGesture {
-                        
-                                isOpen = true
-                                
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-                                    withAnimation(.easeOut(duration: 0.5)) {
-                                        showCard = true
-                                    }
-                                }
-                            }
-                    } else {
-                        
-                        ZStack {
-                            Image("OpenEnvelopeFrame")
+            } else {
+                
+                VStack(spacing: 30) {
+                    
+                    Text(isOpen ? "" : "Click To Open!")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.bold)
+                        .transition(.opacity)
+                        .offset(x: 0, y: 80)
+                    
+                    ZStack {
+                        if !isOpen {
+                            
+                            Image("CloseEnvelopeFrame")
                                 .resizable()
                                 .scaledToFit()
-                                .frame(width: 420)
-                                .offset(y: 40)
-                                .blur(radius: showCard ? 8 : 0)
-                                .animation(.easeInOut(duration: 0.5), value: showCard)
-                            
-                            if showCard {
-                                
-                                VStack {
-                                    if let question = viewModel.currentQuestion?.question {
-                                        Text(question)
-                                            .font(.system(size: 20, weight: .medium, design: .rounded))
-                                            .multilineTextAlignment(.center)
-                                            .padding(30)
-                                    } else {
-                                        Text("Loading")
-                                            .font(.system(size: 40, weight: .medium, design: .rounded))
+                                .frame(width: 420, height: 420)
+                                .onTapGesture {
+                                    
+                                    isOpen = true
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                                        withAnimation(.easeOut(duration: 0.5)) {
+                                            showCard = true
+                                        }
                                     }
                                 }
-                                .frame(width: 300, height: 200)
-                                .background(Color.white)
-                                .cornerRadius(20)
-                                .shadow(radius: 10)
-                                .offset(y: 50)
-                                .transition(.move(edge: .bottom).combined(with: .opacity))
+                        } else {
+                            
+                            ZStack {
+                                Image("OpenEnvelopeFrame")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 420)
+                                    .offset(y: 40)
+                                    .blur(radius: showCard ? 8 : 0)
+                                    .animation(.easeInOut(duration: 0.5), value: showCard)
+                                
+                                if showCard {
+                                    
+                                    VStack {
+                                        if let question = viewModel.currentQuestion?.question {
+                                            Text(question)
+                                                .font(.system(size: 20, weight: .medium, design: .rounded))
+                                                .multilineTextAlignment(.center)
+                                                .padding(30)
+                                        } else {
+                                            Text("Loading")
+                                                .font(.system(size: 40, weight: .medium, design: .rounded))
+                                        }
+                                    }
+                                    .frame(width: 300, height: 200)
+                                    .background(Color.white)
+                                    .cornerRadius(20)
+                                    .shadow(radius: 10)
+                                    .offset(y: 50)
+                                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                                }
                             }
                         }
                     }
-                }
-                
-                Spacer()
-                
-                if showCard {
-                    Button(action: {
-                        
-                            showCard = false
-                            isOpen = false
-                        
-                        viewModel.nextQuestion()
-                    }) {
-                        Text("Next Question")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .clipShape(Capsule())
+                    
+                    Spacer()
+                    
+                    if showCard {
+                        Button(action: {
+                            
+                            //                            showCard = false
+                            //                            isOpen = false
+                            
+                            viewModel.nextQuestion()
+                        }) {
+                            Text("Next Question")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Capsule())
+                        }
+                        .padding(.horizontal, 40)
+                        .padding(.bottom, 30)
                     }
-                    .padding(.horizontal, 40)
-                    .padding(.bottom, 30)
                 }
             }
         }
-//        .navigationTitle(viewModel.levelTitle)
-//        .navigationBarTitleDisplayMode(.inline)
-//        .navigationBarBackButtonHidden(true)
-//        .toolbar {
-//            ToolbarItem(placement: .navigationBarLeading) {
-//                Button(action: { dismiss() }) {
-//                    Image(systemName: "chevron.left")
-//                        .fontWeight(.bold)
-//                }
-//            }
-//        }
+        //        .navigationTitle(viewModel.levelTitle)
+        //        .navigationBarTitleDisplayMode(.inline)
+        //        .navigationBarBackButtonHidden(true)
+        //        .toolbar {
+        //            ToolbarItem(placement: .navigationBarLeading) {
+        //                Button(action: { dismiss() }) {
+        //                    Image(systemName: "chevron.left")
+        //                        .fontWeight(.bold)
+        //                }
+        //            }
+        //        }
         
-        .navigationDestination(isPresented: $viewModel.isFinished) {
-            CongratulationsView(finishedLevel: viewModel.currentLevel)
-        }
         .onAppear {
-            // Pas halaman muncul, baru suruh VM siapin soal sesuai levelnya
             viewModel.startLevel(selectedLevel)
+        }
+    }
+    
+    var congratBodyText: String {
+        if selectedLevel == 3 {
+            return "You have finished this section. Hope you now know each other better!"
+        } else {
+            return "You have finished this section. You may level up to explore deeper topics if you feel comfortable."
         }
     }
 }
