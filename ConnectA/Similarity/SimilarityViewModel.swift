4464898bded17f2ae3150f8ff8b7d9a2665e1934
Michael <micm3335@gmail.com>
2026-04-09 21:11:07 +0700

    feat: add change color to button

--- ConnectA/Similarity/SimilarityViewModel.swift
+++ ConnectA/Similarity/SimilarityViewModel.swift
@@ -10,12 +10,54 @@ import Combine
 class SimilarityViewModel: ObservableObject {
     @Published var allTags: [Tag] = []
     @Published var selectedTags: Set<Tag> = []
-    
+    @Published var player1SelectedTags: Set<String> = []
+    @Published var player2SelectedTags: Set<String> = []
+
     init() {
         loadTags()
     }
     
     func loadTags() {
         allTags = loadTagsFromJSON()
+    }
+    var isPlayer1Ready: Bool {
+        !player1SelectedTags.isEmpty
+    }
+        
+    var isPlayer2Ready: Bool {
+        !player2SelectedTags.isEmpty
+    }
+        
+    var similarTags: [String] {
+        Array(player1SelectedTags.intersection(player2SelectedTags)).sorted()
+    }
+
+    func togglePlayer1Tag(_ tag: String) {
+        if player1SelectedTags.contains(tag) {
+            player1SelectedTags.remove(tag)
+        } else {
+            player1SelectedTags.insert(tag)
+        }
+    }
+    
+    func togglePlayer2Tag(_ tag: String) {
+        if player2SelectedTags.contains(tag) {
+            player2SelectedTags.remove(tag)
+        } else {
+            player2SelectedTags.insert(tag)
+        }
+    }
+        
+    func isSelectedByPlayer1(_ tag: String) -> Bool {
+        player1SelectedTags.contains(tag)
+    }
+        
+    func isSelectedByPlayer2(_ tag: String) -> Bool {
+        player2SelectedTags.contains(tag)
+    }
+        
+    func reset() {
+        player1SelectedTags = []
+        player2SelectedTags = []
     }
 }
